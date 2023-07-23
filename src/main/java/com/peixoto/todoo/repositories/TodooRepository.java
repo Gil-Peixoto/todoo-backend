7bb312e80565960cf682498e234ea8e23bafc2bf
Gil-Peixoto <araujo.gil27@gmail.com>
2023-07-23 16:25:17 -0300

    Criado Entity, Repository e config DB h2

---
+++ src/main/java/com/peixoto/todoo/repositories/TodooRepository.java
@@ -0,0 +1,11 @@
+package com.peixoto.todoo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.peixoto.todoo.entities.Todoo;
+
+@Repository
+public interface TodooRepository extends JpaRepository<Todoo, Integer>{
+
+}
