c261e92a6fc9d9f185693db36294199ec6ec9196
Gil-Peixoto <araujo.gil27@gmail.com>
2023-07-23 11:49:27 -0300

    projeto todoo criado

---
+++ src/test/java/com/peixoto/todoo/TodooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peixoto.todoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
